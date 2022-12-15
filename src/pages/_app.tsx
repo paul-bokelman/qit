165c10d5f118ef723a0a2444384491751ee162b9
Paul Bokelman <paul.bokelman1@gmail.com>
2022-12-14 17:14:19 -0800

    chore: clean up repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,9 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import "tailwindcss/tailwind.css";
 
-import { trpc } from "../utils/trpc";
+import { trpc } from "@/utils/trpc";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+const QueueIt: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
@@ -16,4 +16,4 @@ const MyApp: AppType<{ session: Session | null }> = ({
   );
 };
 
-export default trpc.withTRPC(MyApp);
+export default trpc.withTRPC(QueueIt);
